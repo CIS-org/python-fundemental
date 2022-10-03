03568b0be34eff950c8e290e783a71755158e23f
gunsr <gunsrokect@gmail.com>
2022-10-03 17:37:12 +0700

    inisial

---
+++ main.py
@@ -0,0 +1,17 @@
+# Sekuensial
+print('Ibu berkata, "Pergi ke toko')
+print('Budi menjawab , "Baik, apa yang harus saya lakukan di toko?')
+print('Ibu menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print('Maka budi berangkat ke toko')
+print ('Dan mulai belanja')
+
+# Percabangan
+milk_bottle_count =  173
+
+if milk_bottle_count >0:
+    print('Budi mengecek harganya, dan ternyata uangnya cukup')
+    print('Budi membeli 1 botol susu')
+else:
+    print('Budi tidak jadi membeli 1 botol susu')
+
+print('Budi pulang ke rumah menyerahkan hasil kepada Ibu')
