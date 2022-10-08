0655da2ebe665def3276c00035d1832faf5a12ab
gunsr <gunsrokect@gmail.com>
2022-10-08 19:34:24 +0700

    pengulangan

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ print('Maka budi berangkat ke toko')
 print ('Dan mulai belanja')
 
 # Percabangan
-milk_bottle_count =  173
-
+milk_bottle_count = 173
+egg_count = 1587
+print(f'Jummlah Botol Susu {milk_bottle_count} btl' )
 if milk_bottle_count >0:
     print('Budi mengecek harganya, dan ternyata uangnya cukup')
-    print('Budi membeli 1 botol susu')
+    if egg_count == 0:
+        print('Budi membeli 1 botol susu')
+    else:
+        print('Budi membeli 6 botol susu')
 else:
     print('Budi tidak jadi membeli 1 botol susu')
 
 print('Budi pulang ke rumah menyerahkan hasil kepada Ibu')
+
